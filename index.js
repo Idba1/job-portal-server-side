3b5e2ae76dfa266a5a6cafb9412bf4f5afb631c2
Idba1 <moniraislamself@gmail.com>
2024-05-11 12:47:42 +0600

    connect with mongodb database

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 9000
 const app = express()
 
+
+
 const corsOptions = {
     origin: [
         'http://localhost:5173',
-        'http://localhost:5174',,
+        'http://localhost:5174', ,
     ],
     credentials: true,
     optionSuccessStatus: 200,
@@ -15,12 +18,32 @@ const corsOptions = {
 app.use(cors(corsOptions))
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wwse58h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Hey This is JobNest Server!')
 })
-
-
 
 
 app.listen(port, () => console.log(`Server running on port ${port}`))

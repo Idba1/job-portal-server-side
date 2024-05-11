1bb3ef9cae401237f1c7c4fddaab4d553d96be3d
Idba1 <moniraislamself@gmail.com>
2024-05-11 13:12:47 +0600

    create alljob data collection

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
+        const alljobCollection = client.db('jobnest').collection('alljob')
+
+        // Get alljob data from mongo
+        app.get('/alljob', async (req, res) => {
+            const result = await alljobCollection.find().toArray()
+            res.send(result)
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
